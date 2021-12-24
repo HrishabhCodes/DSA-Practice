e1b96109747f268d183713865f837c4989ba91ba
Hrishabh Jain <hrishabh507@gmail.com>
2021-12-24 23:51:02 +0530

    Add solutions of questions on arrays

---
+++ 1. Kunal Kushwaha - DSA Bootcamp/assignments/Arrays/39. FirstMissingPositive.java
@@ -0,0 +1,33 @@
+package com.hrishabh;
+
+class _41_FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = {3,4,-1,1};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    static public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[correct] != nums[i]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
